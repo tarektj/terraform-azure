3173685b5d326d2e773aaa2ff9c29e70b2cbc27a
ilkay <ilkay.mo.tj@gmail.com>
2022-08-08 11:29:09 +0000

    add NIC

---
+++ NIC.tf
@@ -0,0 +1,17 @@
+resource "azurerm_network_interface" "myNIC" {
+  name                = var.nicname
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.myTerraformNetwork.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.myPublicIP.id
+  }
+  tags = var.tags
+}
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.myNIC.id
+  network_security_group_id = azurerm_network_security_group.myTerraformNSG.id
+}
