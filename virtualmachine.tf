789f42a88f6f10a88f0ffd454b784fd97dc8bfc0
ilkay <ilkay.mo.tj@gmail.com>
2022-08-08 12:44:52 +0000

    add virtalmachine and ssh-key

---
+++ virtualmachine.tf
@@ -0,0 +1,29 @@
+resource "azurerm_linux_virtual_machine" "myVirtualMachine" {
+  name                = var.virtualMachineName
+  resource_group_name = azurerm_resource_group.myTerrformGroup.name
+  location            = var.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.myNIC.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.ssh-key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  computer_name = var.virtualMachineName
+  disable_password_authentication = true
+}
